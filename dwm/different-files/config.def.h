7e87d78ec328f5b530d81d8496de72f4df0d7d3d
Steven Fuchs <stevencfuchs@icloud.com>
2025-06-04 07:15:12 -0500

    Updated Custom config.def.h
    This step was to get ready for the creation
    of the diff file for the individual software.

--- dwm/different-files/config.def.h
+++ dwm/different-files/config.def.h
@@ -61,45 +61,23 @@ static const Layout layouts[] = {
 /* commands */
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
 static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, NULL };
-static const char *termcmd[]  = { "alacritty", NULL };
-static const char *emailcmd[] = { "geary", NULL };
+static const char *termcmd[]  = { "xfce4-terminal", NULL };
+static const char *emailcmd[] = { "thunderbird", NULL };
 static const char *filecmd[] = { "thunar", NULL };
-static const char *lockcmd[] = { "i3lock-fancy", NULL };
+static const char *lockcmd[] = { "light-locker-command -l", NULL };
 static const char *browsercmd[] = { "microsoft-edge", NULL};
-static const char *up_vol[] = {
-    "/bin/sh", "-c",
-    "amixer set Master 5%+ && notify-send \"Volume Level\" \"$(amixer -D default -M get Master | awk 'NR==6 {print $5}' | sed -e 's/\\[//g' -e 's/\\]//g')\"",
-    NULL
-};
-
-static const char *down_vol[] = {
-    "/bin/sh", "-c",
-    "amixer set Master 5%- && notify-send \"Volume Level\" \"$(amixer -D default -M get Master | awk 'NR==6 {print $5}' | sed -e 's/\\[//g' -e 's/\\]//g')\"",
-    NULL
-};
-static const char *mute_vol[] = {
-    "/bin/sh", "-c",
-    "amixer set Master toggle && notify-send \"Volume Level\" \"$(amixer -D default -M get Master | awk 'NR==6 {print $5}' | sed -e 's/\\[//g' -e 's/\\]//g')\"",
-    NULL
-};
-
-static const char *brighter[] = {
-    "/bin/sh", "-c",
-    "brightnessctl set 10%+ && notify-send \"Screen Brightness\" \"$(brightnessctl intel_backlight | awk 'NR==2 {print $4}' | sed -e 's/\\[//g' -e 's/\\]//g')\"",
-    NULL
-};
-static const char *dimmer[]   = {
-    "/bin/sh", "-c",
-    "brightnessctl set 10%- && notify-send \"Screen Brightness\" \"$(brightnessctl intel_backlight | awk 'NR==2 {print $4}' | sed -e 's/\\[//g' -e 's/\\]//g')\"",
-    NULL
-};
+static const char *up_vol[] = { "amixer set Master 5%+", NULL };
+static const char *down_vol[] = { "amixer set Master 5%-", NULL };
+static const char *mute_vol[] = { "amixer set Master toggle", NULL };
+static const char *brighter[] = { "brightnessctl set 10%+", NULL };
+static const char *dimmer[]   = { "brightnessctl set 10%-", NULL };
 static const Key keys[] = {
 	/* modifier                     key        function        argument */
 	{ MODKEY,                       XK_p,      spawn,          {.v = dmenucmd } },
 	{ MODKEY|Mod1Mask,              XK_l,      spawn,          {.v = lockcmd } },
-	{ MODKEY,			XK_b,	   spawn,          {.v = browsercmd } },
-	{ MODKEY,	                XK_Return, spawn,          {.v = termcmd } },
-        { MODKEY,                       XK_l,      spawn,          {.v = emailcmd } },
+	{ MODKEY,						XK_b,	   spawn,          {.v = browsercmd } },
+	{ MODKEY,	                    XK_Return, spawn,          {.v = termcmd } },
+    { MODKEY,                       XK_l,      spawn,          {.v = emailcmd } },
 	{ MODKEY,                       XK_f,      spawn,          {.v = filecmd } },
 	{ MODKEY|ShiftMask,             XK_b,      togglebar,      {0} },
 	{ MODKEY,                       XK_j,      focusstack,     {.i = +1 } },
@@ -122,12 +100,12 @@ static const Key keys[] = {
 	{ MODKEY,                       XK_period, focusmon,       {.i = +1 } },
 	{ MODKEY|ShiftMask,             XK_comma,  tagmon,         {.i = -1 } },
 	{ MODKEY|ShiftMask,             XK_period, tagmon,         {.i = +1 } },
-	{ 0, 				XF86XK_AudioMute,        spawn, {.v = mute_vol } },
-        { 0, 				XF86XK_AudioLowerVolume, spawn, {.v = down_vol } },
-        { 0, 				XF86XK_AudioRaiseVolume, spawn, {.v = up_vol } },
-	{ 0,                            0x1008FFB2,        	 spawn, {.v = mute_vol } },
-	{ 0, 				XF86XK_MonBrightnessDown, spawn, {.v = dimmer } },
-        { 0, 				XF86XK_MonBrightnessUp,   spawn, {.v = brighter } },
+	{ 0, 							XF86XK_AudioMute,        spawn, {.v = mute_vol } },
+    { 0, 							XF86XK_AudioLowerVolume, spawn, {.v = down_vol } },
+    { 0, 							XF86XK_AudioRaiseVolume, spawn, {.v = up_vol } },
+	{ 0,                            XF86XK_AudioMute,        spawn, {.v = mute_vol } },
+	{ 0, 							XF86XK_MonBrightnessDown, spawn, {.v = dimmer } },
+    { 0, 							XF86XK_MonBrightnessUp,   spawn, {.v = brighter } },
 	TAGKEYS(                        XK_1,                      0)
 	TAGKEYS(                        XK_2,                      1)
 	TAGKEYS(                        XK_3,                      2)
